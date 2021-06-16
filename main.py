e07a03f15528c27cf5c2bdcc261bc46982811a41
Cesare Magnetti <cm1320@gpu09.doc.ic.ac.uk>
2021-06-16 12:14:10 +0100

    added training options and fixed some minor bugs.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from environment.baseEnvironment import BaseEnvironment
 from agent.agent import Agent
 from logger.logger import Log, Logger
+from options.options import gather_options, print_options
 import argparse
 from tqdm import tqdm
 from moviepy.editor import ImageSequenceClip
@@ -9,7 +10,7 @@ import numpy as np
 import torch
 
 # parsing arguments
-parser = argparse.ArgumentParser(description='test script to verify we can sample trajectories from the environment.')
+parser = argparse.ArgumentParser(description='train/test scripts to launch navigation experiments.')
 # directories handling
 parser.add_argument('--dataroot', '-r',  type=str, help='path to the XCAT CT volumes.')
 parser.add_argument('--name', '-n', type=str, default='sample_experiment', help='name of the experiment.')
@@ -20,28 +21,34 @@ parser.add_argument('--results_dir', type=str, default='./results/', help='where
 parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints/', help='where to save the trajectory.')
 # training options
 parser.add_argument('--train', action='store_true', help='if training the agent before testing it.')
-parser.add_argument('--batch_size', type=int, default=64, help="batch size for the replay buffer.")
+parser.add_argument('--load', type=str, default=None, help='which model to load from.')
+parser.add_argument('--batch_size', type=int, default=128, help="batch size for the replay buffer.")
 parser.add_argument('--buffer_size', type=int, default=int(1e6), help="capacity of the replay buffer.")
 parser.add_argument('--gamma', type=int, default=0.99, help="discount factor.")
 parser.add_argument('--tau', type=int, default=1e-3, help="weight for soft update of target parameters.")
-parser.add_argument('--learning_rate', '-lr', type=float, default=5e-4, help="learning rate for the q network.")
+parser.add_argument('--learning_rate', '-lr', type=float, default=0.0002, help="learning rate for the q network.")
 parser.add_argument('--update_every', type=int, default=4, help="how often to update the network, in steps.")
-parser.add_argument('--exploring_steps', type=int, default=10000, help="number of purely exploring steps at the beginning.")
+parser.add_argument('--exploring_steps', type=int, default=50000, help="number of purely exploring steps at the beginning.")
 parser.add_argument('--action_size', type=int, default=6, help="how many action can a single agent perform.\n(i.e. up/down,left/right,forward/backwards = 6 in a 3D volume).")
 parser.add_argument('--n_agents', type=int, default=3, help="how many RL agents (heads) will share the same CNN backbone.")
 parser.add_argument('--n_episodes', type=int, default=2000, help="number of episodes to train the agents for.")
-parser.add_argument('--n_steps_per_episode', type=int, default=250, help="number of steps in each episode.")
+parser.add_argument('--n_steps_per_episode', type=int, default=500, help="number of steps in each episode.")
 parser.add_argument('--eps_start', type=float, default=1.0, help="epsilon factor for egreedy policy, starting value.")
 parser.add_argument('--eps_end', type=float, default=0.01, help="epsilon factor for egreedy policy, starting value.")
 parser.add_argument('--eps_decay', type=float, default=0.995, help="epsilon factor for egreedy policy, decay factor.")
 parser.add_argument('--reward_id', type=int, default=2885, help="ID of the anatomical structure of interest. (default: left ventricle, 2885)")
+parser.add_argument('--penalty_per_step', type=float, default=0.1, help="give a small penalty for each step to incentivize moving towards planes of interest.")
+parser.add_argument('--no_area_penalty', action='store_true', help='by default we incentivize the agents to maximize the area of the triangle they span.\n'\
+                                                                   'This is to prevent them from moving towards the edges of a volume, which are meaningless.')
 parser.add_argument('--no_scale_intensity', action='store_true', help="If you do not want to scale the intensities of the CT volume.")
 parser.add_argument('--loss', default=torch.nn.SmoothL1Loss(), help="torch.nn instance of the loss function to use while training the Qnetwork.")
 # random seed for reproducibility
 parser.add_argument('--seed', type=int, default=1, help="random seed for reproducibility.")
 
+#parser = gather_options()
 args = parser.parse_args()
 args.use_cuda = torch.cuda.is_available()
+#print_options(args, parser)
 
 def train(parser):
     """Deep Q-Learning training.
@@ -53,12 +60,14 @@ def train(parser):
         parser.eps_end (float): minimum value of epsilon
         parser.eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
     """
-    rewards, TDerrors = Log("rewards", parser.n_episodes, parser.n_steps_per_episode), Log("TDerrors", parser.n_episodes, parser.n_steps_per_episode)
-    logger = Logger(os.path.join(args.checkpoints_dir, args.name), rewards, TDerrors)
+    rewards = Log("rewards", parser.n_episodes, parser.n_steps_per_episode)
+    TDerrors = Log("TDerrors", parser.n_episodes, parser.n_steps_per_episode)
+    epsilon = Log("epsilon", parser.n_episodes)
+    logger = Logger(os.path.join(args.checkpoints_dir, args.name), rewards, TDerrors, epsilon)
     # initialize epsilon
     eps = parser.eps_start 
-    print_every = 10
-    for episode in range(1, parser.n_episodes+1):
+    print_every = 100
+    for episode in tqdm(range(1, parser.n_episodes+1)):
         env.reset()
         state = env.state
         for _ in range(parser.n_steps_per_episode):
@@ -73,6 +82,7 @@ def train(parser):
             rewards.push(reward)
             if TDerror: # TD error can be None if agent.update_every>1
                 TDerrors.push(TDerror)
+        epsilon.push(eps)
 
         if episode % print_every == 0:
             print("[{}/{}] ({:.0f}%) eps:{:.3f}\n\t\ttotal reward collected in the last episode: {:.2f}\n\t\t"\
@@ -105,7 +115,7 @@ def test(parser):
     for i in tqdm(range(parser.n_steps_per_episode)):
         actions = agent.act(state)
         state, reward = env.step(actions)
-        frames.append(env.render(state, with_seg=True, titleText='time step: {} reward:{:.5f}'.format(i+1, reward)))
+        frames.append(env.render(state, titleText='reward:{:.5f}'.format(reward)))
   
     # save all frames as a GIF
     if not os.path.exists(os.path.join(args.results_dir, args.name)):
@@ -122,6 +132,8 @@ if __name__=="__main__":
     env = BaseEnvironment(args)
     # instanciate agent
     agent = Agent(env, args)
+    if args.load is not None:
+        agent.load(args.load)
 
     # train agent
     if args.train:
