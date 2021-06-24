fixed no gradient progation through other heads.

agent/agent.py
@@ -114,8 +114,8 @@ class Agent(BaseAgent):
         # concatenate and move to gpu
         states = torch.from_numpy(np.vstack(planes[:self.config.batch_size])).float().to(self.config.device)
         next_states = torch.from_numpy(np.vstack(planes[self.config.batch_size:])).float().to(self.config.device)
-        rewards = torch.tensor(rewards).repeat(self.n_agents).float().to(self.config.device) # repeat for n_agents that share this reward
-        actions = torch.from_numpy(np.hstack(actions)).flatten().unsqueeze(0).long().to(self.config.device) # unroll for each agent one by one
+        rewards = torch.tensor(rewards).float().to(self.config.device)
+        actions = torch.from_numpy(np.hstack(actions)).unsqueeze(-1).long().to(self.config.device)
         batch = (states, actions, rewards, next_states)
 
         # 2. make a training step (retain graph because we will backprop multiple times through the backbone cnn)

agent/trainers.py
@@ -1,4 +1,5 @@
 import torch
+import concurrent.futures
 
 class DeepQLearning():
     def __init__(self, gamma):
@@ -21,28 +22,29 @@ class DeepQLearning():
         """
         # 1. split batch
         states, actions, rewards, next_states = batch
-        #print("training batch")
-        #print(states.shape, actions.shape, rewards.shape, next_states.shape)
-
         # 2. get our value estimates Q for the current state, and our target values estimates (Qtarget) for the next state
-        # note that the qnetworks have multiple heads, returned as a list, concatenate them
-        # so that the shape is (n_agents*batch_size) X action_size
-        Q = torch.cat(local_model(states), dim=0) 
-        Qtarget = torch.cat(target_model(next_states), dim=0)
-        #print("Q and MaxQ:", Q.shape, Qtarget.shape)
-        # 3. gather Q values for the actions taken at the current state
-        Q = Q.gather(1, actions).squeeze()
-        #print("gathered Q values: ", Q.shape)
-        # 4. get the target value of the greedy action at the next state
-        MaxQ = Qtarget.max(1)[0].detach()
-        #print("greedy target values: ", MaxQ.shape)
-        # 5. backup the expected value of this action by bootstrapping on the greedy value of the next state
-        Qhat = rewards + self.gamma*MaxQ
-        #print("rewards + self.gamma*MaxQ: ", rewards.shape, MaxQ.shape, Qhat.shape)
-        # evalauate TD error as a fit function for the netwrok
-        loss = criterion(Q, Qhat)
-        #print("loss: ", loss.shape)
+        # note that the qnetworks have multiple heads, returned as a list, we will need to process each head separately,
+        # and aggregate the individual losses
+        Q = local_model(states)
+        Qtarget = target_model(next_states)
+        # 3. evaluate the loss (TDerror) for each head, launch in parallel for efficiency, aggregate each loss by summation
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.single_head_loss, q, q_target, act, rewards, criterion) for q, q_target, act in zip(Q, Qtarget, actions.permute(2,0,1))]
+        # 4. aggregate the losses of each head
+        loss = sum([f.result() for f in futures])
         return loss
+
+    def single_head_loss(self, Q, Qtarget, actions, rewards, criterion):
+        # 1. gather the values of the action taken
+        Q = Q.gather(1, actions).squeeze()
+        # 2. get the target value of the greedy action at the next state
+        MaxQ = Qtarget.max(1)[0].detach()
+        # 3. backup the expected value of this action by bootstrapping on the greedy value of the next state
+        Qhat = rewards + self.gamma*MaxQ
+        # 4. evalauate TD error as a fit function for the netwrok
+        loss = criterion(Q, Qhat)
+        return loss
+
 
 class DoubleDeepQLearning():
     def __init__(self, gamma):
@@ -53,7 +55,7 @@ class DoubleDeepQLearning():
         """
         # batch size and discount factor
         self.gamma = gamma
-
+    
     def step(self, batch, local_model, target_model, criterion):
         """Update qbetwork parameters using given batch of experience tuples.
         Params
@@ -65,27 +67,27 @@ class DoubleDeepQLearning():
         """
         # 1. split batch
         states, actions, rewards, next_states = batch
-        #print("training batch")
-        #print(states.shape, actions.shape, rewards.shape, next_states.shape)
-
         # 2. get our value estimates Q for the current state, and our target values estimates (Qtarget) for the next state
-        # note that the qnetworks have multiple heads, returned as a list, concatenate them
-        # so that the shape is (n_agents*batch_size) X action_size
-        Q = torch.cat(local_model(states), dim=0) 
-        Q_next = torch.cat(local_model(next_states), dim=0)
-        Qtarget_next = torch.cat(target_model(next_states), dim=0)
-        #print("Q, Q_next and Qtarget_next:", Q.shape, Q_next.shape, Qtarget_next.shape)
-        # 3. gather Q values for the actions taken at the current state
+        # note that the qnetworks have multiple heads, returned as a list, we will need to process each head separately,
+        # and aggregate the individual losses
+        Q = local_model(states)
+        Q_next = local_model(next_states)
+        Qtarget_next = target_model(next_states)
+        # 3. evaluate the loss (TDerror) for each head, launch in parallel for efficiency, aggregate each loss by summation
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(self.single_head_loss, q, q_next, q_target_next, act, rewards, criterion) for q, q_next, q_target_next, act in zip(Q, Q_next, Qtarget_next, actions)]
+        # 4. aggregate the losses of each head
+        loss = sum([f.result() for f in futures])
+        return loss
+
+    def single_head_loss(self, Q, Q_next, Qtarget_next, actions, rewards, criterion):
+        # 1. gather Q values for the actions taken at the current state
         Qa = Q.gather(1, actions).squeeze()
-        #print("gathered Q values: ", Qa.shape)
-        # 4. get the discrete action that maximizes the target value at the next state
+        # 2. get the discrete action that maximizes the target value at the next state
         a_next = Qtarget_next.max(1)[1].unsqueeze(0)
         Qa_next = Q_next.gather(1, a_next).detach().squeeze()
-        #print("estimated target values: ", Qa_next.shape)
-        # 5. backup the expected value of this action by bootstrapping on the greedy value of the next state
+        # 3. backup the expected value of this action by bootstrapping on the greedy value of the next state
         Qhat = rewards + self.gamma*Qa_next
-        #print("rewards + self.gamma*Qa_next: ", rewards.shape, Qa_next.shape, Qhat.shape)
-        # evalauate TD error as a fit function for the netwrok
+        # 4. evalauate TD error as a fit function for the network
         loss = criterion(Qa, Qhat)
-        #print("loss: ", loss.shape)
         return loss