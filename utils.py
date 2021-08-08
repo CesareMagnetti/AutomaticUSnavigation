2471bda94cee334d6d68ff36cca87b66395f5182
Cesare Magnetti <cm1320@gpu11.doc.ic.ac.uk>
2021-08-08 14:43:28 +0100

    fixed visualization bug.

--- utils.py
+++ utils.py
@@ -75,7 +75,7 @@ def train(config, local_model, target_model, wandb_entity="us_navigation", sweep
                         for key, log in out.items():
                             wandb.log(log["wandb"], commit=True)
                             # animate the trajectory followed by the agent in the current episode
-                            visualizer.render_frames(log["planes"], "episode%d.gif"%episode)
+                            visualizer.render_frames(log["planes"], n_rows = 2 if agent.config.location_aware else 1, fname = "episode%d.gif"%episode)
                         # upload file to wandb
                         wandb.save(os.path.join(visualizer.savedir, "episode%d.gif"%episode))
         # at the end of the training session save the model as .onnx to improve the open sourceness and exchange-ability amongst different ML frameworks
