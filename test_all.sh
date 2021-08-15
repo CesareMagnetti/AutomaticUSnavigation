9885e36d70d8770ff4e24061babf2b8f05dfcb9b
Cesare Magnetti <cm1320@gpu08.doc.ic.ac.uk>
2021-08-15 17:33:48 +0100

    added all initial experiments to test

--- test_all.sh
+++ test_all.sh
@@ -5,3 +5,19 @@ python test_new.py --name 15Volume_planeDistance_terminateOscillate --load lates
 # test 15Volume_anatomy_terminateOscillate both easy and full objectives
 python test_new.py --name 15Volume_anatomy_terminateOscillate --load latest --volume_ids samp0,samp1,samp2,samp3,samp4,samp15,samp16,samp17,samp18,samp19 --n_runs 100 --fname quantitative_metrics
 python test_new.py --name 15Volume_anatomy_terminateOscillate --load latest --volume_ids samp0,samp1,samp2,samp3,samp4,samp15,samp16,samp17,samp18,samp19 --n_runs 100 --fname quantitative_metrics --easy_objective
+
+# test 15Volume_both_terminateOscillate both easy and full objectives
+python test_new.py --name 15Volume_both_terminateOscillate --load latest --volume_ids samp0,samp1,samp2,samp3,samp4,samp15,samp16,samp17,samp18,samp19 --n_runs 100 --fname quantitative_metrics
+python test_new.py --name 15Volume_both_terminateOscillate --load latest --volume_ids samp0,samp1,samp2,samp3,samp4,samp15,samp16,samp17,samp18,samp19 --n_runs 100 --fname quantitative_metrics --easy_objective
+
+# test 15Volume_locationAware_planeDistance_terminateOscillate both easy and full objectives
+python test_new.py --name 15Volume_locationAware_planeDistance_terminateOscillate --load latest --volume_ids samp0,samp1,samp2,samp3,samp4,samp15,samp16,samp17,samp18,samp19 --n_runs 100 --fname quantitative_metrics
+python test_new.py --name 15Volume_locationAware_planeDistance_terminateOscillate --load latest --volume_ids samp0,samp1,samp2,samp3,samp4,samp15,samp16,samp17,samp18,samp19 --n_runs 100 --fname quantitative_metrics --easy_objective
+
+# test 15Volume_locationAware_anatomy_terminateOscillate both easy and full objectives
+python test_new.py --name 15Volume_locationAware_anatomy_terminateOscillate --load latest --volume_ids samp0,samp1,samp2,samp3,samp4,samp15,samp16,samp17,samp18,samp19 --n_runs 100 --fname quantitative_metrics
+python test_new.py --name 15Volume_locationAware_anatomy_terminateOscillate --load latest --volume_ids samp0,samp1,samp2,samp3,samp4,samp15,samp16,samp17,samp18,samp19 --n_runs 100 --fname quantitative_metrics --easy_objective
+
+# test 15Volume_locationAware_both_terminateOscillate both easy and full objectives
+python test_new.py --name 15Volume_locationAware_both_terminateOscillate --load latest --volume_ids samp0,samp1,samp2,samp3,samp4,samp15,samp16,samp17,samp18,samp19 --n_runs 100 --fname quantitative_metrics
+python test_new.py --name 15Volume_locationAware_both_terminateOscillate --load latest --volume_ids samp0,samp1,samp2,samp3,samp4,samp15,samp16,samp17,samp18,samp19 --n_runs 100 --fname quantitative_metrics --easy_objective
