e6018371836674b4f761ea428fa93a759082f2e9
Cesare Magnetti <cm1320@gpu09.doc.ic.ac.uk>
2021-08-22 17:32:05 +0100

    adapted code for easy objective experiment on rotated volumes

--- options/options.py
+++ options/options.py
@@ -48,12 +48,12 @@ def gather_options(phase="train"):
     parser.add_argument('--termination_oscillation_freq', type=int, default=3, help="if in the last ``termination_history_len`` steps there are more than this number of equal planes, terminate the episode for oscillation.")
     
     # reward signal shaping
-    parser.add_argument('--goal_centroids', type=str, default="2885,2897,2893", help="centroids of these anatomical tissues will define the goal plane.\n"\
-                                                                                     "(default: LV+RV+LA: 2885,2897,2893). if multiple IDs separate by comma.")
+    parser.add_argument('--goal_centroids', type=str, default="2885,2897,2895", help="centroids of these anatomical tissues will define the goal plane.\n"\
+                                                                                     "(default: LV+RV+RA: 2885,2897,2895). if multiple IDs separate by comma.")
     parser.add_argument('--planeDistanceRewardWeight', type=float, default=1., help='relative weight of the plane distance reward if present, see rewards/rewards.py for more info.\n')                                                                                 
-    parser.add_argument('--anatomyRewardWeight', type=float, default=1., help='relative weight of the anatomy reward if present, see rewards/rewards.py for more info.\n')
-    parser.add_argument('--anatomyRewardIDs', type=str, default="2885,2897,2893", help="segmentation IDs for the anatomical reward, see rewards/rewards.py for more info.\n"\
-                                                                             "(default: LV+RV+LA: 2885,2897,2893). if multiple IDs separate by comma.")
+    parser.add_argument('--anatomyRewardWeiscreenshots_NLP_lectureght', type=float, default=1., help='relative weight of the anatomy reward if present, see rewards/rewards.py for more info.\n')
+    parser.add_argument('--anatomyRewardIDs', type=str, default="2885,2897,2895", help="segmentation IDs for the anatomical reward, see rewards/rewards.py for more info.\n"\
+                                                                             "(default: LV+RV+RA: 2885,2897,2895). if multiple IDs separate by comma.")
     parser.add_argument('--incrementalAnatomyReward', action='store_true', default=True, help="whether the agent is rewarded on the improvement of anatomical content or on the current anatomical content.")
     parser.add_argument('--oobReward', type=float, default=0.01, help='penalize each agent if it steps outside the boundaries of the volume, see rewards/rewards.py for more info.')
     parser.add_argument('--areaRewardWeight', type=float, default=0.01, help='reward the agents if they stay far apart from each other (measuring area of spanned triangle), see rewards/rewards.py for more info.\n'\
