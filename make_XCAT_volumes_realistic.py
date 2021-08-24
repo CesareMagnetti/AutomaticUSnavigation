fddce7d466916332dfc727ddb527a52a743e288e
Cesare Magnetti <cm1320@gpu13.doc.ic.ac.uk>
2021-08-24 10:37:58 +0100

    updating code ...

--- make_XCAT_volumes_realistic.py
+++ make_XCAT_volumes_realistic.py
@@ -15,6 +15,7 @@ import argparse
 import SimpleITK as sitk
 import numpy as np
 import os
+from tqdm import tqdm
 
 import torchvision.transforms as transforms
 import torchvision.models as models
@@ -67,8 +68,8 @@ class Normalization(nn.Module):
         # .view the mean and std to make them [C x 1 x 1] so that they can
         # directly work with image Tensor of shape [B x C x H x W].
         # B is batch size. C is number of channels. H is height and W is width.
-        self.mean = torch.tensor(mean).view(-1, 1, 1)
-        self.std = torch.tensor(std).view(-1, 1, 1)
+        self.mean = mean.detach().clone().view(-1, 1, 1)
+        self.std = std.detach().clone().view(-1, 1, 1)
 
     def forward(self, img):
         # normalize img
@@ -141,14 +142,14 @@ def get_input_optimizer(input_img):
     optimizer = optim.LBFGS([input_img.requires_grad_()])
     return optimizer
 
-def run_style_transfer(cnn, normalization_mean, normalization_std, content_img, style_img, input_img, num_steps=300, style_weight=1000, content_weight=1):
+def run_style_transfer(cnn, normalization_mean, normalization_std, content_img, style_img, input_img, num_steps=300, style_weight=10000, content_weight=1):
     """Run the style transfer."""
-    print('Building the style transfer model..')
+
     model, style_losses, content_losses = get_style_model_and_losses(cnn,
         normalization_mean, normalization_std, style_img, content_img)
+
     optimizer = get_input_optimizer(input_img)
 
-    print('Optimizing..')
     run = [0]
     while run[0] <= num_steps:
 
@@ -173,11 +174,11 @@ def run_style_transfer(cnn, normalization_mean, normalization_std, content_img, 
             loss.backward()
 
             run[0] += 1
-            if run[0] % 50 == 0:
-                print("run {}:".format(run))
-                print('Style Loss : {:4f} Content Loss: {:4f}'.format(
-                    style_score.item(), content_score.item()))
-                print()
+            # if run[0] % 50 == 0:
+            #     print("run {}:".format(run))
+            #     print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+            #         style_score.item(), content_score.item()))
+            #     print()
 
             return style_score + content_score
 
@@ -202,50 +203,67 @@ def intensity_scaling(ndarr, pmin=None, pmax=None, nmin=None, nmax=None):
     ndarr = ndarr*(nmax-nmin)+nmin
     return ndarr
 
-def load(filename, pmin, pmax, nmin, nmax, main_ax):
+def load(filename, pmin, pmax, nmin, nmax):
     itkVolume = sitk.ReadImage(filename)
     Volume = sitk.GetArrayFromImage(itkVolume) 
     # preprocess volume
     Volume = Volume/Volume.max()*255
     Volume = intensity_scaling(Volume, pmin=pmin, pmax=pmax, nmin=nmin, nmax=nmax)
     Volume = Volume.astype(np.uint8)
-    # roll volume axis as queried (to slice volume along x,y or z)
-    np.rollaxis(Volume, main_ax)
     return Volume
 
 # ==== handle style transfer for each slice of a volume ====
+class AddGaussianNoise(object):
+    def __init__(self, mean=0., std=1.):
+        self.std = std
+        self.mean = mean
+        
+    def __call__(self, tensor):
+        return tensor + torch.randn(tensor.size()) * self.std + self.mean
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
+
 totensor = transforms.ToTensor()
+resize = transforms.Resize(256)
+noise = AddGaussianNoise(mean=0, std=0.5)
+noise1 = AddGaussianNoise(mean=0, std=0.1)
 def NST_Volume(Volume, style_img, main_ax, init="content"):
     # 1. roll volume axis as queried (to slice volume along x,y or z)
     np.rollaxis(Volume, main_ax)
+    print(Volume.shape)
     sx,sy,sz = Volume.shape
-    assert sx == sy  and sy == sz, "can only translate cubic volumes"
     # 2. loop through all slices along main_Axis, transfer the style and update slices in the Volume
-    for i in range(sx):
+    for i in tqdm(range(100, sx), "transfering volume ..."):
         # 2.1 setup content and input images to tensor already sets image in (0,1) range
-        content_img = totensor(Volume[i, ...]).unsqueeze(0).unsqueeze(0).to(device, torch.float)
+        content_img = noise1(totensor(Volume[i, ...])).unsqueeze(0).to(device, torch.float)
         if init == "content":
-            input_img = content_img.clone()
+            input_img = noise(totensor(Volume[i, ...])).unsqueeze(0).to(device, torch.float)
         else:
             input_img = torch.randn(content_img.data.size(), device=device)
-        
         # 2.2 run style transfer on this plane
         output = run_style_transfer(cnn, cnn_normalization_mean, cnn_normalization_std, content_img, style_img, input_img)
-
+        output = output.cpu().detach().numpy().squeeze()
+        img = content_img.cpu().detach().numpy().squeeze()
+        img = np.hstack([img, output])
+        # save as we go to inspect (DELETE AFTER)
+        if not os.path.exists("./temp_transferred_slices"):
+            os.makedirs("./temp_transferred_slices")
+        plt.imsave("./temp_transferred_slices/sample{}.png".format(i), img, cmap="Greys_r")
         # 2.3 store output image in the volume
-        Volume[i, ...] = output.cpu().detach().numpy().squeeze()
+        Volume[i, ...] = output
 
     # 3. roll the axis back to its origin form
     np.rollaxis(Volume, 0, main_ax)
-
     return Volume
 
 
 
 parser = argparse.ArgumentParser(description='train/test scripts to launch navigation experiments.')
-parser.add_argument('--dataroot', '-r',  type=str, default="/vol/biomedic3/hjr119/DATA/XCAT_VOLUMES/", help='path to the XCAT CT volumes.')
+parser.add_argument('--dataroot', '-r',  type=str, default="/vol/biomedic3/hjr119/XCAT/generation2", help='path to the XCAT CT volumes.')
 parser.add_argument('--saveroot', '-s',  type=str, default="./XCAT_VOLUMES_NST/", help='path to the XCAT CT volumes.')
 parser.add_argument('--volume_ids', '-vol_ids', type=str, default='samp0', help='filename(s) of the CT volume(s) comma separated.')
+parser.add_argument('--style_img', '-si', type=str, default='./LIDC-IDRI-0139_70.jpg', help='path to style image.')
 parser.add_argument('--main_ax', type=int, default=0, help="main_ax to follow when translating each slice of the volume.")
 parser.add_argument('--init', '-i',  type=str, default="content", help='initialize input image either to [content] or [random] image.')
 parser.add_argument('--pmin', type=int, default=150, help="pmin value for intensity_scaling() function.")
@@ -254,22 +272,43 @@ parser.add_argument('--nmin', type=int, default=0, help="nmin value for intensit
 parser.add_argument('--nmax', type=int, default=255, help="nmax value for intensity_scaling() function.")
 config = parser.parse_args()
 
-if __name__ == "main":
-    vol_ids = config.split(",")
+if __name__ == "__main__":
+    vol_ids = config.volume_ids.split(",")
     # main function to transfer a volume
     def main(vol_id):
         # 1. load and preprocess the XCAT volume
-        Volume = load(os.path.join(config.dataroot, vol_id+"_1_CT.nii.gz"))
+        Volume = load(os.path.join(config.dataroot, vol_id+"_1_CT.nii.gz"), config.pmin, config.pmax, config.nmin, config.nmax)
         # 2. load and preprocess the style CT image
-        style_img_numpy = Image.open("./LIDC-IDRI-0139_70.jpg").convert('L')
-        style_img = totensor(style_img_numpy).unsqueeze(0).to(device, torch.float)
+        style_img_numpy = Image.open(config.style_img).convert('L')
+        style_img = resize(totensor(style_img_numpy)).unsqueeze(0).to(device, torch.float)
         # 3. transfer the volume
         TransferredVolume = NST_Volume(Volume, style_img, config.main_ax, init=config.init)
-        return TransferredVolume
+        # 4. save volume
+        # ======== THESE PARAMS ARE TAKEN FROM vol/biomedic3/hjr119/XCAT/gen_xcat.py =======
+        ZOOM = 3
+        BASE = 256/ZOOM # Default
+        CM_TO_PIX = 0.3125*BASE
+        final_dim = 256
+        dim_2_percentage_stop  = .65 # haut bas
+        dim_2_percentage_start = 0. # 0. - 0.75
+        plane_dim = int(np.ceil(final_dim / (dim_2_percentage_stop-dim_2_percentage_start)))
+        pix_res = CM_TO_PIX/plane_dim
+        # =======================================================================================
+        # Transforming numpy to sitk
+        print("Transforming numpy to sitk...")
+        sitk_arr = sitk.GetImageFromArray(TransferredVolume)
+        sitk_arr.SetSpacing([pix_res, pix_res, pix_res])
+
+        # Save sitk to nii.gz file
+        print("Saving sitk to .nii.gz file...", )
+        writer = sitk.ImageFileWriter()
+        if not os.path.exists(config.saveroot):
+            os.makedirs(config.saveroot)
+        writer.SetFileName(os.path.join(config.saveroot, vol_id+"_1_CT.nii.gz"))
+        writer.Execute(sitk_arr)
 
     # parallelize across all input volumes
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = {vol_id: executor.submit(main, vol_id) for vol_id in vol_ids}
-    TransferredVolumes = {vol_id: f.result() for vol_id, f in futures.items()}
+        futures = [executor.submit(main, vol_id) for vol_id in vol_ids]
 
 
